0c84e3dc7553aed1b3f451e2ec0acd836f3c3ff1
Deepa-Senthil <deepasenthil5812@gmail.com>
2024-09-23 14:56:41 +0530

    send the email with the order details and orderd items

--- controllers/api/cartItemController.js
+++ controllers/api/cartItemController.js
@@ -1,37 +1,127 @@
-const express = require("express");
-const router = express.Router();
 const CartItem = require("../../database/models/cartItem");
+const nodemailer = require("nodemailer");
 
-// POST endpoint to save cart items
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "deepa.tamtree@gmail.com",
+    pass: "llbf upiq gaxj hqxy",
+  },
+});
+
 exports.createCartItems = async (req, res) => {
   try {
-    const cartItems = req.body.cartItems;
-    console.log(cartItems);
+    const { cartItems, paymentData } = req.body;
+
+    if (!cartItems || cartItems.length === 0) {
+      return res.status(400).json({ error: "Cart items are required" });
+    }
 
     await CartItem.insertMany(cartItems);
+    try {
+      const cartItemsTable = cartItems
+        .map(
+          (item) => `
+      <tr>
+        <td>${item.title}</td>
+        <td>${item.quantity}</td>
+        <td>${item.price.toFixed(2)}</td>
+      </tr>
+    `
+        )
+        .join("");
+
+      const paymentDataHtml = `
+       <p><strong>First Name:</strong> ${paymentData.firstName}</p>
+      <p><strong>Last Name:</strong> ${paymentData.lastName}</p>
+      <p><strong>Delivery Date:</strong> ${new Date(
+        paymentData.deliveryDate
+      ).toLocaleDateString()}</p>
+      <p><strong>  DeliveryOption:</strong> ${paymentData.deliveryOption}</p>
+     <p><strong>Phone Number:</strong> ${paymentData.phoneNumber}</p>
+     <p><strong>Address:</strong> ${paymentData.address}</p>
+      <p><strong>Email:</strong> ${paymentData.email}</p>
+      <p><strong>Amount:</strong> $${(paymentData.amount / 100).toFixed(2)}</p>
+    `;
+      // Email content for the user
+      const userMailOptions = {
+        from: process.env.EMAIL_USER,
+        to: paymentData.email,
+        subject: "Payment Confirmation",
+        html: `
+        <p>Dear ${paymentData.firstName} ${paymentData.lastName},</p>
+        <p>Thank you for your purchase! Your payment of $${(
+          paymentData.amount / 100
+        ).toFixed(2)} was successful.</p>
+        <h3>Order Details:</h3>
+          ${paymentDataHtml}
+           <h3>Ordered Items:</h3>
+        <table border="1" cellpadding="5" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${cartItemsTable}
+          </tbody>
+        </table>
+        <p>Best regards,<br>Sindhus kitchen</p>
+      `,
+      };
+
+      // Email content for the created email
+      const createdMailOptions = {
+        from: paymentData.email,
+        to: process.env.EMAIL_USER,
+        subject: "New Order Received",
+        html: `
+        <h3>New order received:</h3>
+        <p>Order Details:</p>
+       ${paymentDataHtml}
+        <h3>Ordered Items:</h3>
+        <table border="1" cellpadding="5" cellspacing="0">
+          <thead>
+            <tr>
+              <th>Item Name</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${cartItemsTable}
+          </tbody>
+        </table>
+      `,
+      };
+
+      // Send email to the user
+      transporter.sendMail(userMailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email to user:", error.message);
+        } else {
+          console.log("Email sent to user:", info.response);
+        }
+      });
+
+      // Send email to the created email
+      transporter.sendMail(createdMailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email to created email:", error.message);
+        } else {
+          console.log("Email sent to created email:", info.response);
+        }
+      });
+    } catch (error) {
+      console.error("Error retrieving cart item:", error.message);
+    }
     res.status(201).json({ message: "Cart items saved successfully" });
   } catch (error) {
-    res.status(500).json({ error: "Failed to save cart items" });
-  }
-};
-
-exports.getLastCreatedCartItem = async (req, res) => {
-  console.log("start");
-
-  try {
-    console.log("start");
-    const lastCreatedCartItem = await CartItem.find().sort({
-      createdAt: -1,
-    });
-
-    if (!lastCreatedCartItem) {
-      return res.status(404).json({ error: "No cart items found" });
-    }
-    // res.status(200).json({ lastCreatedCartItem });
-    return lastCreatedCartItem;
-  } catch (error) {
+    console.error("Error saving cart items:", error);
     res
       .status(500)
-      .json({ error: "Failed to retrieve the last created cart item" });
+      .json({ error: "Failed to save cart items", details: error.message });
   }
 };
--- controllers/api/paymentController.js
+++ controllers/api/paymentController.js
@@ -4,32 +4,13 @@
  */
 const Payment = require("../../database/models/payment");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-const nodemailer = require("nodemailer");
-const cartItemController = require("../api/cartItemController");
 /**
  * @param {Request} req - The Express request object
  * @param {Response} res - The Express response object
  */
 
-const transporter = nodemailer.createTransport({
-  service: "Gmail", // Or any other email service you are using
-  auth: {
-    user: "deepa.tamtree@gmail.com",
-    pass: "llbf upiq gaxj hqxy", // Make sure this is correct
-  },
-});
 
-const handleCartItem = async (req, res) => {
-  try {
-    const cartResponse = await cartItemController.getLastCreatedCartItem(
-      req,
-      res
-    );
-    // Ensure no further response is sent after this point
-  } catch (error) {
-    res.status(500).json({ error: "Error handling cart item" });
-  }
-};
+
 
 exports.createPaymentIntent = async (req, res) => {
   const {
@@ -45,14 +26,13 @@ exports.createPaymentIntent = async (req, res) => {
   } = req.body;
 
   try {
-    // Create the payment intent with Stripe
+    
     const paymentIntent = await stripe.paymentIntents.create({
-      amount, // amount should be in cents
+      amount, 
       currency: "usd",
       payment_method_types: ["card"],
     });
 
-    // Save the payment details in your database
     const transaction = new Payment({
       firstName,
       lastName,
@@ -60,88 +40,26 @@ exports.createPaymentIntent = async (req, res) => {
       phoneNumber,
       email,
       deliveryOption,
-      amount, // Amount is in cents
+      amount, 
       paymentId: paymentIntent.id,
       postalCode,
-      status: paymentIntent.status, // This might initially be 'requires_confirmation'
+      status: paymentIntent.status,
       deliveryDate: new Date(deliveryDate),
       createdAt: new Date(),
     });
-    console.log("transaction Details", transaction);
+   
 
     await transaction.save();
 
-    // Extract order details
     const { paymentId, status, ...orderDetails } = transaction.toObject();
 
-    // Respond with the client secret to confirm payment on the frontend
     res.status(200).send({
       clientSecret: paymentIntent.client_secret,
       message: "Payment intent created and saved successfully",
     });
     console.log("status", paymentIntent.status);
 
-    // Send email after payment success
     if (paymentIntent.status === "requires_payment_method") {
-      try {
-        console.log("start to get cart item");
-        try {
-          var cartResponse = await cartItemController.getLastCreatedCartItem();
-          console.log("cartResponse", cartResponse);
-          var cartItem = cartResponse;
-        } catch (error) {
-          console.log("cartResponseerror", cartResponse);
-        }
-
-        console.log("end to get cart item");
-
-        console.log("cartItem", cartItem);
-
-        // Email content for the user
-        const userMailOptions = {
-          from: process.env.EMAIL_USER,
-          to: email,
-          subject: "Payment Confirmation",
-          text: `Dear ${firstName} ${lastName},\n\nThank you for your purchase! Your payment of $${(
-            amount / 100
-          ).toFixed(2)} was successful.\n\nBest regards,\nSindhus kitchen`,
-        };
-
-        // Email content for the created email
-        const createdMailOptions = {
-          from: email,
-          to: process.env.EMAIL_USER,
-          subject: "New Order Received",
-          text: `New order received:\n\nOrder Details:\n${JSON.stringify(
-            orderDetails,
-            null,
-            2
-          )}`,
-        };
-
-        // Send email to the user
-        transporter.sendMail(userMailOptions, (error, info) => {
-          if (error) {
-            console.error("Error sending email to user:", error.message);
-          } else {
-            console.log("Email sent to user:", info.response);
-          }
-        });
-
-        // Send email to the created email
-        transporter.sendMail(createdMailOptions, (error, info) => {
-          if (error) {
-            console.error(
-              "Error sending email to created email:",
-              error.message
-            );
-          } else {
-            console.log("Email sent to created email:", info.response);
-          }
-        });
-      } catch (error) {
-        console.error("Error retrieving cart item:", error.message);
-      }
     }
   } catch (error) {
     console.error("Error creating payment intent:", error.message);
